931d4eceecf9db4dc801a84d3bca2d3c5ca3cddc
Yevheniia Bohdan <AlmDomini@gmail.com>
2022-11-20 19:57:50 +0100

    task1

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -3,3 +3,33 @@ import { galleryItems } from './gallery-items';
 // Change code below this line
 
 console.log(galleryItems);
+
+// Описаний в документації
+import SimpleLightbox from 'simplelightbox';
+// Додатковий імпорт стилів
+import 'simplelightbox/dist/simple-lightbox.min.css';
+
+const gallery = document.querySelector('.gallery');
+
+const galleryList = createGalleryCard(galleryItems);
+gallery.insertAdjacentHTML('beforeend', galleryList);
+
+function createGalleryCard(cardItems) {
+  return cardItems
+    .map(({ preview, original, description }) => {
+      return `
+        <a class="gallery__item"
+        href="${original}">
+        <img class="gallery__image"
+            src="${preview}" 
+            alt=${description}" />
+        </a>
+        `;
+    })
+    .join('');
+}
+
+new SimpleLightbox('.gallery a', {
+  captionsData: 'alt',
+  captionDelay: 250,
+});
