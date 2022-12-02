c86b8979924e5896fc692aa3e41d8309973e994f
Yevheniia Bohdan <AlmDomini@gmail.com>
2022-12-02 20:31:21 +0100

    v3 done

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -11,29 +11,30 @@ console.log(refs.textarea);
 console.log(refs.email);
 
 refs.form.addEventListener('submit', onFormSubmit);
-refs.form.addEventListener('input', throttle(onTextareaInput), 500);
+refs.form.addEventListener('input', throttle(onTextareaInput, 500));
 
-const formData = { email: '', message: '' };
+let formData = {};
 
 previosData();
 
 function onFormSubmit(evt) {
   evt.preventDefault();
   evt.currentTarget.reset();
+  console.log(JSON.parse(localStorage.getItem(STORAGE_KEY)));
   localStorage.removeItem(STORAGE_KEY);
-  console.log('отправляємо, скидаємо, очищаємо');
 }
 
 function onTextareaInput(evt) {
-  formData[evt.target.name] = evt.target.value;
-  console.log(formData);
+  let fg = JSON.parse(localStorage.getItem(STORAGE_KEY));
+  formData = { ...fg, [evt.target.name]: evt.target.value };
   localStorage.setItem(STORAGE_KEY, JSON.stringify(formData));
 }
+
 function previosData() {
   const parseData = JSON.parse(localStorage.getItem(STORAGE_KEY));
   if (parseData) {
     console.log(parseData);
-    refs.textarea.value = parseData.message;
-    refs.email.value = parseData.email;
+    refs.textarea.value = parseData.message || '';
+    refs.email.value = parseData.email || '';
   }
 }
